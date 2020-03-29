7a20c2fe6384cf968414a38fc57547a54f314706
koftov <ahronlu@gmail.com>
2020-03-29 18:53:04 +0300

    moved our directory andshop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -12,28 +12,28 @@ import {
 import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
-  <div className="checkout-page">
-    <div className="checkout-header">
-      <div className="header-block">
+  <div className='checkout-page'>
+    <div className='checkout-header'>
+      <div className='header-block'>
         <span>Product</span>
       </div>
-      <div className="header-block">
+      <div className='header-block'>
         <span>Description</span>
       </div>
-      <div className="header-block">
+      <div className='header-block'>
         <span>Quantity</span>
       </div>
-      <div className="header-block">
+      <div className='header-block'>
         <span>Price</span>
       </div>
-      <div className="header-block">
+      <div className='header-block'>
         <span>Remove</span>
       </div>
     </div>
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">TOTAL: ${total}</div>
+    <div className='total'>TOTAL: ${total}</div>
   </div>
 );
 
